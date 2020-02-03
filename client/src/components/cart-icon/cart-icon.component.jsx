970962d090ddb2b306d28c261f3e16735ffbaf86
RohanN <42676702+Rohanhustle@users.noreply.github.com>
2020-02-03 18:19:30 +0530

    Update cart-icon.component.jsx

--- client/src/components/cart-icon/cart-icon.component.jsx
+++ client/src/components/cart-icon/cart-icon.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
+
 import {
   CartContainer,
   ShoppingIcon,
