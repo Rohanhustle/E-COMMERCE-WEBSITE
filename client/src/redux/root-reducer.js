aff81bb59f430a5e65f8e88eb512324c69524d34
RohanN <42676702+Rohanhustle@users.noreply.github.com>
2020-02-03 18:18:35 +0530

    Update root-reducer.js

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer';
